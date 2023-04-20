a42d3be44efc6a27cd4c991ba8492e8d51c48d3d
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-20 16:40:12 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function fibonacci(num) {
-// your code here
-}
+  // memoize previous results
+  const memo = {};
 
-module.exports = fibonacci;
+  function fibonacciHelper(num) {
+    // check memoization cache
+    if (memo[num] !== undefined) {
+      return memo[num];
+    }
+
+    // base cases
+    if (num === 0) {
+      return 0;
+    } else if (num === 1) {
+      return 1;
+    }
+
+    // recursive case
+    else {
+      const result = fibonacciHelper(num - 1) + fibonacciHelper(num - 2);
+      memo[num] = result; // memoize result
+      return result;
+    }
+  }
+
+  return fibonacciHelper(num);
+}
+// calculate the 10th Fibonacci number (should be 55)
+console.log(fibonacci(num));
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
